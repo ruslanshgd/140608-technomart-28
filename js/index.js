0b9a6e6f80e678c8a03acfe9bc4a4312c094d764
Ruslan <ruslanshgd@gmail.com>
2020-06-14 12:25:59 +0300

    Верстка и интерактив для карты
    Сверстал, стилизовал и оживил карту на главной странице по клику на картинку карты

--- js/index.js
+++ js/index.js
@@ -49,3 +49,26 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+var mapLink = document.querySelector(".contacts-link-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
